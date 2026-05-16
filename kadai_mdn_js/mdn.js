f23b23b1c427a057c8c4b69c1cfcfc55d5c15a7c
hiroyuki20260401 <hiroyuki1131fp@gmail.com>
2026-05-16 16:56:58 +0900

    フォルダーをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const date = new Date(2024, 9, 12); 
+
+const year = date.getFullYear(); 
+const month = date.getMonth() + 1;
+const day = date.getDate(); 
+
+console.log(`${year}年${month}月${day}日`); 
