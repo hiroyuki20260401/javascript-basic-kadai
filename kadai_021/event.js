788ad86d290c7d669f00917ba8b52acef46129c8
hiroyuki20260401 <hiroyuki1131fp@gmail.com>
2026-05-21 17:27:11 +0900

    フォルダーをアップロード

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+// HTML要素をIDで取得します
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+// ボタンがクリックされた時のイベントを設定します
+btn.addEventListener('click', () => {
+  // 2000ミリ秒（2秒）後に実行する処理を指定します
+  setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+});
